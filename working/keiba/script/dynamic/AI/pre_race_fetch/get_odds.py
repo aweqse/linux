7a30e949cdb60d0917d81661ea71d2685e4be484
aweqse <aweqse@gmail.com>
2025-07-11 10:32:13 +0900

    新規作成

---
+++ working/keiba/script/dynamic/AI/pre_race_fetch/get_odds.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from time import sleep
+import re
+import csv
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+options.add_argument('--disable-gpu')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--allow-running-insecure-content')
+options.add_argument('--disable-web-security')
+options.add_argument('--blink-settings=imagesEnabled=false')
+options.add_argument('--ignore-certificate-errors')
+options.add_argument('--no-sandbox')
+
+driver = webdriver.Chrome(options=options)
+
+
+
+
+
+
+
+
